ecd9b4059d851b707855ab2fc119091e1ab8d754
yamina <yami.hidaoui92@gmail.com>
2014-03-09 11:59:25 +0100

    dernier modification

--- src/Pointage/userBundle/Controller/DefaultController.php
+++ src/Pointage/userBundle/Controller/DefaultController.php
@@ -9,5 +9,12 @@ class DefaultController extends Controller
     public function indexAction($name)
     {
         return $this->render('userBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('userBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('userBundle:Default:index.html.twig', array('name' => $name));
     }
+    public function pointerAction()
+    {
+        return $this->render('userBundle:Default:pointer.html.twig');
+    }
+
 }
